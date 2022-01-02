8e6607b1197e8d1a5ebd7b64c040cfee293e76d4
Gustavo Branco <guster32@gmail.com>
2022-01-02 12:11:08 -0800

    Simple check capabilities

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,34 @@
-extern crate nix;
 
-extern crate libloading;
-extern crate khronos_egl as egl;
+/// Check the `util` module to see how the `Card` structure is implemented.
+pub mod utils;
+use utils::*;
 
-use nix::fcntl::{ open, OFlag };
-use nix::sys::stat::Mode;
-use gbm_sys::{ gbm_device};
+pub fn main() {
+    let card = Card::open_global();
 
-fn main() {
+    // Attempt to acquire and release master lock
+    println!("Get Master lock: {:?}", card.acquire_master_lock());
+    println!("Release Master lock: {:?}", card.release_master_lock());
 
-    let lib = unsafe { libloading::Library::new("libEGL.so.1").expect("unable to find libEGL.so.1") }; 
-    let egl = unsafe { egl::DynamicInstance::<egl::EGL1_5>::load_required_from(lib).expect("unable to load libEGL.so.1") };
-    
-    let input_fd = open("/dev/dri/card0", OFlag::O_RDWR, Mode::empty())
-         .unwrap();
-    let gbm_device:*mut gbm_device = unsafe { gbm_sys::gbm_create_device(input_fd) };
-    let attr:&[usize] = &[0];
-    let egl_dpy = egl.get_platform_display(0x31D7, gbm_device as *mut std::ffi::c_void, attr).unwrap();
-    let _res = egl.initialize(egl_dpy);
-    println!("Hello, world! res is OKay!");
-    
+    // Get the Bus ID of the device
+    println!("Getting Bus ID: {:?}", card.get_bus_id().unwrap().as_ref());
+
+    // Figure out driver in use
+    println!("Getting driver info");
+    let driver = card.get_driver().unwrap();
+    println!("\tName: {:?}", driver.name());
+    println!("\tDate: {:?}", driver.date());
+    println!("\tDesc: {:?}", driver.description());
+
+    // Enable all possible client capabilities
+    println!("Setting client capabilities");
+    for &cap in capabilities::CLIENT_CAP_ENUMS {
+        println!("\t{:?}: {:?}", cap, card.set_client_capability(cap, true));
+    }
+
+    // Get driver capabilities
+    println!("Getting driver capabilities");
+    for &cap in capabilities::DRIVER_CAP_ENUMS {
+        println!("\t{:?}: {:?}", cap, card.get_driver_capability(cap));
+    }
 }
---
+++ src/utils/mod.rs
@@ -0,0 +1,71 @@
+#![allow(dead_code)]
+
+extern crate drm;
+
+pub use drm::control::Device as ControlDevice;
+pub use drm::Device;
+
+pub use drm::control::property::*;
+pub use drm::control::ResourceHandle;
+
+#[derive(Debug)]
+/// A simple wrapper for a device node.
+pub struct Card(std::fs::File);
+
+/// Implementing  `AsRawFd` is a prerequite to implementing the traits found
+/// in this crate. Here, we just calling `as_raw_fd()` on the inner File.
+impl std::os::unix::io::AsRawFd for Card {
+    fn as_raw_fd(&self) -> std::os::unix::io::RawFd {
+        self.0.as_raw_fd()
+    }
+}
+
+/// With `AsRawFd` implemented, we can now implement `drm::Device`.
+impl Device for Card {}
+impl ControlDevice for Card {}
+
+/// Simple helper methods for opening a `Card`.
+impl Card {
+    pub fn open(path: &str) -> Self {
+        let mut options = std::fs::OpenOptions::new();
+        options.read(true);
+        options.write(true);
+        Card(options.open(path).unwrap())
+    }
+
+    pub fn open_global() -> Self {
+        Self::open("/dev/dri/card0")
+    }
+}
+
+pub mod capabilities {
+    use drm::ClientCapability as CC;
+    pub const CLIENT_CAP_ENUMS: &[CC] = &[CC::Stereo3D, CC::UniversalPlanes, CC::Atomic];
+
+    use drm::DriverCapability as DC;
+    pub const DRIVER_CAP_ENUMS: &[DC] = &[
+        DC::DumbBuffer,
+        DC::VBlankHighCRTC,
+        DC::DumbPreferredDepth,
+        DC::DumbPreferShadow,
+        DC::Prime,
+        DC::MonotonicTimestamp,
+        DC::ASyncPageFlip,
+        DC::CursorWidth,
+        DC::CursorHeight,
+        DC::AddFB2Modifiers,
+        DC::PageFlipTarget,
+        DC::CRTCInVBlankEvent,
+        DC::SyncObj,
+    ];
+}
+
+// pub mod images {
+//     use image;
+
+//     pub fn load_image(name: &str) -> image::RgbaImage {
+//         let path = format!("examples/images/{}", name);
+//         image::open(path).unwrap().to_rgb8()
+//     }
+
+// }
